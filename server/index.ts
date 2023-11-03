b6ee4de5f432e415ffbffe8763a453468be2dd30
Zaki <reborn7875@gmail.com>
2023-11-03 17:14:34 +0800

    Add swagger

--- server/index.ts
+++ server/index.ts
@@ -1,18 +1,24 @@
-import express, { Router } from "express";
-import cookieParser from "cookie-parser";
-import productRouter from "./routes/product.js";
-import userRouter from "./routes/user.js";
-import campaignRouter from "./routes/campaign.js";
-import orderRouter from "./routes/order.js";
-import reportRouter from "./routes/report.js";
-import branch from "./middleware/branch.js";
-import authenticate from "./middleware/authenticate.js";
-import authorization from "./middleware/authorization.js";
-import rateLimiter from "./middleware/rateLimiter.js";
-import { errorHandler } from "./utils/errorHandler.js";
+import express, { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import cookieParser from 'cookie-parser';
+import productRouter from './routes/product.js';
+import userRouter from './routes/user.js';
+import campaignRouter from './routes/campaign.js';
+import orderRouter from './routes/order.js';
+import reportRouter from './routes/report.js';
+import branch from './middleware/branch.js';
+import authenticate from './middleware/authenticate.js';
+import authorization from './middleware/authorization.js';
+import rateLimiter from './middleware/rateLimiter.js';
+import { errorHandler } from './utils/errorHandler.js';
+import * as fs from 'fs';
+import path from 'path';
 
 const app = express();
 const port = 3000;
+const swaggerDocument = JSON.parse(
+  fs.readFileSync(`${path.resolve()}/swagger-output.json`).toString(),
+);
 
 app.use(cookieParser());
 
@@ -26,7 +32,7 @@ router.use(function (req, res, next) {
 
 app.use(express.json());
 
-app.use("/api", rateLimiter, [
+app.use('/api', rateLimiter, [
   productRouter,
   userRouter,
   campaignRouter,
@@ -36,15 +42,16 @@ app.use("/api", rateLimiter, [
 
 app.use(
   branch(
-    (req) => req.path.includes("/admin"),
-    [authenticate, authorization("admin")]
+    (req) => req.path.includes('/admin'),
+    [authenticate, authorization('admin')],
   ),
-  express.static("../client")
+  express.static('../client'),
 );
 
-app.use("/uploads", express.static("./uploads"));
-app.use("/assets", express.static("./assets"));
+app.use('/uploads', express.static('./uploads'));
+app.use('/assets', express.static('./assets'));
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(errorHandler);
 
 app.listen(port, () => {
